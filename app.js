d4504e3977689822b988f404b8cf9a7dc68f2b35
CRISTONI <adonai_cn@hotmail.com>
2022-08-13 00:38:21 -0300

    relação dinamica entre User e Event

--- app.js
+++ app.js
@@ -1,107 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
-const Event = require('./models/event');
-const User = require('./models/users')
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
 
 const app = express();
 
 app.use(bodyParser.json());
 
+
 app.use('/graphql', graphqlHTTP({
-    schema: buildSchema(`
-        type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        type User {
-            _id: ID!
-            email: String!
-            password: String
-        }
-
-        input EventInput {
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        input UserInput {
-            email: String!
-            password: String!
-        }
-
-        type RootQuery {
-            events: [Event!]!
-        }
-
-        type RootMutation {
-            createEvent(eventInput: EventInput): Event
-            createUser(userInput: UserInput): User
-        }
-    
-        schema {
-            query: RootQuery
-            mutation: RootMutation
-        }
-    `),
-    rootValue: {
-        events: () => {
-            return Event.find()
-                .then(events => {
-                    return events.map(event => {
-                        return {...event._doc, _id: event.id };
-                    })
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-        },
-        createEvent: (args) => {
-            const event = new Event({
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: new Date(args.eventInput.date)    
-            });
-            return event
-                .save()
-                .then(result => {
-                    console.log(result);
-                    return {...result._doc, _id: result.id};
-                })
-                .catch(err => {
-                    console.log(err);
-                    throw err;
-                }); 
-            }   
-        }, 
-        createUser: (args) => {            
-            return bcrypt.hash(args.userInput.password, 12)
-                .then(hashedPassword => {
-                    const user = new User({
-                        email: args.userInput.email,
-                        password: hashedPassword
-                    });
-                    return user.save();
-                })
-                .then(result => {
-                    return { ...result._doc, password: null, _id: result.id };
-                })
-                .catch(err => {
-                    throw err;
-                })
-            },
-
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
     graphiql: true
 }));
 
--- models/users.js
+++ models/users.js
@@ -11,7 +11,7 @@ const userSchema = new Schema({
         type: String,
         require: true
     },
-    createEvents: [
+    createdEvents: [
         {
             type: Schema.Types.ObjectId,
             ref: 'Event'
